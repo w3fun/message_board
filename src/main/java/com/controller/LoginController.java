e183d4a7d25ba959943185902253f81cadf88f95
zhang_ocean <1125694337@qq.com>
2018-01-18 11:17:19 +0800

    1.3

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.controller;
 
-import com.component.CheckUserMatch;
 import com.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/controller/RegisterController.java
+++ src/main/java/com/controller/RegisterController.java
@@ -1,13 +1,12 @@
 package com.controller;
 
 import com.component.CheckCode;
-import com.component.CheckUserExist;
-import com.model.User;
 import com.repository.UserRepository;
+import com.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,29 +25,22 @@ public class RegisterController {
     @Autowired
     private CheckCode checkCode;
 
-    @Autowired
-    private CheckUserExist checkUserExist;
-
-    @RequestMapping(value = "/doRegister", method = RequestMethod.POST)
+    @RequestMapping("/doRegister")
     @ResponseBody
-    public int register(HttpServletRequest request) {
+    public Boolean register(HttpServletRequest request) {
 
         String name = request.getParameter("name1");
         String password = request.getParameter("password1");
-        String userCode = request.getParameter("userCode");
+        String userCode = request.getParameter("userCode1");
 
         User user = new User(name, password);
 
-        int code = checkCode.checkCode(request,userCode);
+        Boolean code = checkCode.checkCode(request,userCode);
 
-        int checkUser = checkUserExist.checkUserExist(name);
-        if(code == 0){
-            if(checkUser == 0){
-                userRepository.save(user);
-            } else {
-                return checkUser;
-            }
+        if(code){
+            userRepository.save(user);
         }
+        System.out.println(code.toString());
 
         return code;
     }
